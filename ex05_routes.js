bd687adbe9c13219a41718983fa427094c11ccad
Rafael <rafamsilva@live.com>
2017-04-25 17:23:36 -0300

    express route

---
+++ ex05_routes.js
@@ -0,0 +1,16 @@
+const express = require ('express')
+const router = express.router
+
+router.use((req, res, next) =>{
+    const init = Date.now()
+    next()
+    console.log(`Tempo = ${Date.now() - init} ms.`)
+})
+
+router.get('/produtos/:id',(req, res) => {
+    res.json({id: req.params.id, name: 'Caneta'})
+})
+
+router.get('/clientes/:id', (req, res) => {
+    res.json({id: req.params.id, name: 'João'})
+})
